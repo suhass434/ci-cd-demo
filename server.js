8515a193cb8b38bd120fdb4beae080177e300148
suhas <1ms22is139@msrit.edu>
2025-08-14 16:34:14 +0530

    First commit

--- server.js
+++
@@ -1,5 +0,0 @@
-const app = require('./app');
-const port = process.env.PORT || 3000;
-if (require.main === module) { // only when run directly
-  module.exports = app.listen(port, () => console.log(`App running on ${port}`));
-}
--- tests/app.test.js
+++
@@ -1,8 +0,0 @@
-const request = require('supertest');
-const app = require('../server');
-
-test('GET / should return correct message', async () => {
-  const res = await request(app).get('/');
-  expect(res.statusCode).toBe(200);
-  expect(res.text).toBe('Hello from CI/CD pipeline!');
-});
